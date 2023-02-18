771dc3697a2546e623fc4cccb016097036aa1460
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2023-02-18 21:52:57 +0200

    first-commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
+import { Profile } from 'components/Profile/Profile';
+
+import user from 'data/user';
+import data from 'data/data';
+import { Statistics } from './Statistics/Statistics';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data} title="Upload stats" />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+
+export const Statistics = ({ stats, title }) => {
+  return (
+    <section className="statistics">
+     { title && <h2>{title}</h2>}
+
+      <ul>
+        {stats.map(item => (
+          <li  key={item.id}>
+            <span>{item.label}</span>
+            <span>{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+    stats: PropTypes.arrayOf(
+    PropTypes.shape({
+        id: PropTypes.string.isRequired, 
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number,
+    })
+    )
+}
